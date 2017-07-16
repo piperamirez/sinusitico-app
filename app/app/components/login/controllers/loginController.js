d4a40a3ca481c3dc72589eafd6f39b9b8a0470aa
Felipe Ramirez <feliperamirezg@gmail.com>
2017-07-16 09:18:04 -0400

    added password validation

--- app/app/components/login/controllers/loginController.js
+++ app/app/components/login/controllers/loginController.js
@@ -3,12 +3,21 @@ angular.module('login.controller', [])
 .controller('LoginController', function($scope, $state, LoginServicesFactory, Storage) {
 
   $scope.loginData = {}
+  $scope.showLoginError = false
 
   $scope.doLogin = function() {
+    $scope.showLoginError = false
+    if ($scope.pass != 'pass') {
+      $scope.showLoginError = true
+      return
+    }
     LoginServicesFactory.postLogin($scope.loginData).then(
       function(response) {
         Storage.setObject('sinusitico.currentUser', response)
         $state.go('main.app.discover')
+      },
+      function(error) {
+        $scope.showLoginError = true
       }
     )
   }
