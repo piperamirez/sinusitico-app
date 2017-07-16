d75d97bfa5e934a210ff909c3e4f4e759050670e
Felipe Ramirez <feliperamirezg@gmail.com>
2017-07-15 21:06:39 -0400

    added map for leads

--- app/app/shared/navbar/controllers/navbarController.js
+++ app/app/shared/navbar/controllers/navbarController.js
@@ -1,6 +1,6 @@
 angular.module('navbar.controller', [])
 
-.controller('NavbarController', function($scope, $modal, LeadServicesFactory) {
+.controller('NavbarController', function($scope, $modal, $timeout, LeadServicesFactory) {
 
   $scope.leadData = {}
 
@@ -10,6 +10,45 @@ angular.module('navbar.controller', [])
     ]
   }
 
+  function drawMap(position) {
+
+    $timeout(function() {
+      this.map = new google.maps.Map(document.getElementById("map"), {
+        center: new google.maps.LatLng(
+          position.lat,
+          position.lng
+        ),
+        zoom: 13,
+        disableDefaultUI: true
+      })
+      var marker = new google.maps.Marker({
+        position: position,
+        map: map
+      })
+    }, 500)
+  }
+
+  function setupMap() {
+    /* // TODO this shit is not working in safari, so we hard-code our current location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        function(position) {
+          $scope.leadData.position = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+          drawMap($scope.leadData.position)
+        }
+      )
+    }
+    */
+    $scope.leadData.position = {
+      lat: -33.416429,
+      lng: -70.589791
+    }
+    drawMap($scope.leadData.position)
+  }
+
   $scope.newLead = function() {
     $scope.newLeadModal = $modal.open({
       scope: $scope,
@@ -17,6 +56,7 @@ angular.module('navbar.controller', [])
       templateUrl: 'app/components/leads/views/newLeadModalView.html',
       size: 'md'
     })
+    setupMap()
   }
 
   $scope.postNewLead = function() {
