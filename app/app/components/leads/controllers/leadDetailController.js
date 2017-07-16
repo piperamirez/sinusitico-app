6db0013975b525d30b92162b2a08d7aa1b9e246a
Felipe Ramirez <feliperamirezg@gmail.com>
2017-07-16 09:29:23 -0400

    dummy lead management function

--- app/app/components/leads/controllers/leadDetailController.js
+++ app/app/components/leads/controllers/leadDetailController.js
@@ -3,7 +3,7 @@ angular.module('lead.detail.controller', [])
 .controller('LeadDetailController', function($scope, $state, $timeout, $stateParams) {
 
   $scope.lead = $stateParams.lead
-  //$scope.lead = {"id": 20, "name": "Andr\u00e9s Errazuriz", "pub_date": "2017-06-17T00:00:00Z", "email": "Andr\u00e9s.Errazuriz@gmail.com", "enterprise": 5, "location": {"lat": 11, "lng": 10}, "role": "Gerente de Operaciones", "address": null, "phone": "562 7384 6428", "tags": null, "facebook_link": null, "linkedin_link": null, "instagram_link": null, "oportunity": null, "close_date": null, "days_pass": 29, "is_premium": true}
+  $scope.lead = {"id": 20, "name": "Andr\u00e9s Errazuriz", "pub_date": "2017-06-17T00:00:00Z", "email": "Andr\u00e9s.Errazuriz@gmail.com", "enterprise": 5, "location": {"lat": 11, "lng": 10}, "role": "Gerente de Operaciones", "address": null, "phone": "562 7384 6428", "tags": null, "facebook_link": null, "linkedin_link": null, "instagram_link": null, "oportunity": null, "close_date": null, "days_pass": 29, "is_premium": true}
 
   function drawMap(position) {
 
@@ -28,5 +28,9 @@ angular.module('lead.detail.controller', [])
   }
 
   setupMap()
+
+  $scope.gestionarLead = function(lead) {
+    $scope.leadGestionado = true
+  }
 
 })
